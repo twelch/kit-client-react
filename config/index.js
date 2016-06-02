1600ea4c7fe8b11d41c11753eddf1e8496b14d2d
Tim Welch <tim.j.welch@gmail.com>
2016-06-02 15:50:09 -0700

    Change default port, add proxy to server API in dev mode

--- config/index.js
+++ config/index.js
@@ -27,7 +27,7 @@ const config = {
   // Server Configuration
   // ----------------------------------
   server_host : localip, // use string 'localhost' to prevent exposure on local network
-  server_port : process.env.PORT || 3000,
+  server_port : process.env.PORT || 3001,
 
   // ----------------------------------
   // Compiler Configuration
