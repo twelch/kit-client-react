7988509e5956d3ea0b3f2eda0f6e23f95c0184b3
Tim Welch <tim.j.welch@gmail.com>
2016-06-05 14:10:16 -0700

    remove ts style type checks

--- src/modules/locale.js
+++ src/modules/locale.js
@@ -7,7 +7,7 @@ export const LOCALE_CHANGE = 'LOCALE_CHANGE'
 // Actions
 // ------------------------------------
 // export const localeChange = createAction(LOCALE_CHANGE, (value) => value)
-export function localeChange (value: string = 'en'): Action {
+export function localeChange (value = 'en') {
   return {
     type: LOCALE_CHANGE,
     payload: value
@@ -22,14 +22,14 @@ export const actions = {
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
-  [LOCALE_CHANGE]: (state: string, action: {payload: string}): string => action.payload
+  [LOCALE_CHANGE]: (state, action) => action.payload
 }
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
 const initialState = navigator.language || navigator.browserLanguage
-export default function localeReducer (state: string = initialState, action: Action): string {
+export default function localeReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
 
   return handler ? handler(state, action) : state
